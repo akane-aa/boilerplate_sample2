cd71ceaad14f8b1922334cb8e4a1492262e199e1
阿部茜 <abeakane@ibm-2dd9a2792d6.people.local>
2018-08-24 18:04:28 +0900

    add

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+onst path = require("path");
+
+module.exports = {
+  mode: "production",
+  //entryパス
+  entry: "./src/main.js",
+  //dist
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  }
+};
